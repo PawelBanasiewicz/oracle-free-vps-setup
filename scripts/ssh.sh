1e3328bc2f4e9d1359b8eca86fefdb48ea9a0635
lukaszdutka <lukaszdutka2@gmail.com>
2025-03-02 17:19:28 +0100

    chore: add comment regarding first ssh run

--- scripts/ssh.sh
+++ scripts/ssh.sh
@@ -4,3 +4,8 @@
 HOST="ubuntu@xxx.xx.xxx.xxx" # IPv4 public address of your instance, see: https://cloud.oracle.com/compute/instances
 
 ssh "$HOST"
+
+# The first time you use ssh you might need to supply the certificate.
+# First, move your private key to this directory (named 'server.key'), then:
+# run this instead (comment previous ssh command, uncomment this command):
+# ssh -i ./server.key "$HOST"
